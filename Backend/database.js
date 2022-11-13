7af7a34cf7ce45be5460309ff17fa16f67c698d3
aditya-sahni <adityasahani976@gmail.com>
2022-11-13 14:11:36 +0530

    Created Express App

---
+++ Backend/database.js
@@ -0,0 +1,9 @@
+//Connection link : mongodb://localhost:27017
+const mongoose = require("mongoose");
+const mongoURL =  "mongodb://localhost:27017";
+const connectToMongo = () =>{
+    mongoose.connect(mongoURL,()=>{
+        console.log("Connected to MongoDB.");
+    })
+}
+module.exports = connectToMongo;
---
+++ Backend/index.js
@@ -0,0 +1,13 @@
+const connectToMongo = require("./database.js")
+connectToMongo();
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
